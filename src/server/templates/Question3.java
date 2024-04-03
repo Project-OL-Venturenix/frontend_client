c13301fc970c5db8b6326cf8168c60b6a7f8bf06
LucasPUN <sypun321@gmail.com>
2024-04-03 14:36:06 +0800

    bug.

---
+++ src/server/templates/Question3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.math.*;
+ public class Question3{
+public double computeTax(int[][] brackets, int income){ 
+ //Enter the code Here.Your class should be named Question3.
+ 
+ }
+}
